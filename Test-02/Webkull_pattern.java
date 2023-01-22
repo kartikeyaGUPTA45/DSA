2a2a02994c0bca075113da40984335a9bce6b7dc
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2023-01-23 04:10:48 +0530

    Update Webkull_pattern.java

--- Test-02/Webkull_pattern.java
+++ Test-02/Webkull_pattern.java
@@ -1,1 +1,42 @@
+import java.util.Scanner;
 
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+
+        // code here
+        
+        for(int i=1; i<=n ; i++){
+            System.out.print("@");
+        }
+        System.out.println();
+        
+        int stars = 1;
+        
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=n-1; j++){
+            System.out.print(" ");
+            }
+            for(int j=1; j<=stars; j++){
+                System.out.print("*");
+            }
+            if(i<=n/2){
+                stars++;
+            }
+            else{
+                stars--;
+            }
+            System.out.println();
+        }
+        
+        
+        
+        for(int i=1; i<=n-2; i++){
+            System.out.print(" ");
+        }
+        for(int i=1; i<=n ; i++){
+            System.out.print("@");
+        }
+    }
+}
